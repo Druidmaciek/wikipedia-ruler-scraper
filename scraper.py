7beafe920402cdc1064ab5f9ec75b451adc5afdc
Maciej <30637919+Druidmaciek@users.noreply.github.com>
2020-03-27 12:35:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
+
 def make_request(url: str) -> str:
     """ Makes a HTTP GET Request.
     """
@@ -10,6 +11,7 @@ def make_request(url: str) -> str:
     if response.status_code == 200:
         return response.content
     raise Exception(f"Request Failed. Status Code {response.status_code} with URL: {url}")
+
 
 def scrape_ruler(url: str) -> dict:
     """Parse Wikipedia ruler page for data."""
@@ -22,8 +24,10 @@ def scrape_ruler(url: str) -> dict:
     data["Ruler"] = data_table.find("tr").text
     return data
 
+
 def scrape_rulers(url: str) -> List[dict]:
+    """ Scrape all rulers from wikipedia rulers page """
     content = make_request(url)
     soup = BeautifulSoup(content, "html.parser")
-    ruler_urls = [x.find("a")["href"] for x in soup.find_all("b")]
+    ruler_urls = ["https://en.wikipedia.org" + x.find("a")["href"] for x in soup.find_all('big') if x.find('a')]
     return [scrape_ruler(ruler_url) for ruler_url in ruler_urls]

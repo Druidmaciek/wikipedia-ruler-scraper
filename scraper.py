4b00e99b3eb90c3d11723673839fbd886b075819
Maciej <30637919+Druidmaciek@users.noreply.github.com>
2020-04-11 10:11:11 +0200

    Update scraper.py
    instead of raising Exception use response.raise_for_status() & use list comprehension instead of filter() + lambda

--- scraper.py
+++ scraper.py
@@ -8,9 +8,8 @@ def make_request(url: str) -> str:
     """ Makes a HTTP GET Request.
     """
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    raise Exception(f"Request Failed. Status Code {response.status_code} with URL: {url}")
+    response.raise_for_status()
+    return response.content
 
 
 def scrape_ruler(url: str) -> dict:
@@ -19,7 +18,7 @@ def scrape_ruler(url: str) -> dict:
     soup = BeautifulSoup(content, "html.parser")
     data_table = soup.find("table", {"class": "infobox vcard"})
     table_rows = data_table.find_all("tr")
-    data_rows = filter(lambda row: row.find("th") and row.find("td"), table_rows)
+    data_rows = [row for row in table_rows if row.find('th') and row.find('td')]
     data = {x.find("th").text: x.find("td").text.strip() for x in data_rows}
     data["Ruler"] = data_table.find("tr").text
     return data
